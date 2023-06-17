98d3d7ec60f27aa0d3b2321f3726351e4faa42bb
PeraSite <pretocki3@gmail.com>
2023-06-17 21:50:01 +0900

    feat: log4net 추가, 기본 GameServer 구조 추가

---
+++ Server/GameServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using log4net;
+
+namespace PeachGame.Server;
+
+public class GameServer : IDisposable {
+	private static readonly ILog Logger = LogManager.GetLogger(typeof(GameServer));
+
+	private readonly TcpListener _server;
+
+	public GameServer(int listenPort) {
+		_server = new TcpListener(IPAddress.Any, listenPort);
+	}
+
+	public void Start() {
+		Logger.Debug($"Server starting...");
+		_server.Start();
+		Logger.Debug($"Server started at {_server.LocalEndpoint}");
+	}
+
+	public void Dispose() {
+		_server.Stop();
+		Logger.Debug($"Server stopped!");
+		GC.SuppressFinalize(this);
+	}
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Text;
+using log4net.Config;
+[assembly: XmlConfigurator(ConfigFile = "log4net.config")]
 
 namespace PeachGame.Server;
 
 internal static class Program {
 	private static void Main() {
+		// 콘솔 입출력 한글 깨짐 수정
+		Console.InputEncoding = Encoding.Unicode;
+		Console.OutputEncoding = Encoding.Unicode;
 
+		// 환경변수 가져오기
+		var listenPort = int.Parse(GetEnvironmentVariable("LISTEN_PORT"));
+
+		// 서버 시작
+		using GameServer server = new GameServer(listenPort);
+		server.Start();
 	}
+
+	private static string GetEnvironmentVariable(string key)
+		=> Environment.GetEnvironmentVariable(key) ?? throw new Exception($"{key} is not set");
 }
