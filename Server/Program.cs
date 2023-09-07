2442853560c2312e7f75d12452c2ed71cc7d6511
PeraSite <pretocki3@gmail.com>
2023-09-07 17:48:10 +0900

    feat: 서버 구동 플랫폼을 Hosting 으로 변경

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,27 +1,50 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using log4net.Config;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 [assembly: XmlConfigurator(ConfigFile = "log4net.config")]
 
 namespace PeachGame.Server;
 
 internal static class Program {
-	private static void Main() {
+	private static async Task Main() {
 		// 콘솔 입출력 한글 깨짐 수정
 		Console.InputEncoding = Encoding.UTF8;
 		Console.OutputEncoding = Encoding.UTF8;
 
+		var host = Host.CreateDefaultBuilder()
+			.ConfigureServices(services => services.AddHostedService<ShutdownService>())
+			.Build();
+
+		await host.RunAsync();
+	}
+}
+
+internal class ShutdownService : IHostedService {
+	private readonly GameServer _server;
+
+	public ShutdownService() {
 		// 환경변수 가져오기
 		var listenPort = int.Parse(GetEnvironmentVariable("LISTEN_PORT"));
 
 		// 서버 시작
-		GameServer server = new GameServer(listenPort);
-		server.Start();
+		_server = new GameServer(listenPort);
+	}
 
-		Console.ReadKey();
+	public Task StartAsync(CancellationToken cancellationToken) {
+		_server.Start();
 
-		// Graceful stop
-		server.Stop();
+		// Await until program exit
+		while (!cancellationToken.IsCancellationRequested) { }
+		return Task.CompletedTask;
+	}
+
+	public Task StopAsync(CancellationToken cancellationToken) {
+		_server.Stop();
+		return Task.CompletedTask;
 	}
 
 	private static string GetEnvironmentVariable(string key)
